fd66e740722c084cc220b783f5b373ccc3ff8e29
Pedro Neto <92586873+Valunah@users.noreply.github.com>
2024-11-07 16:24:05 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -19,5 +19,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region  = "us-east-1"
-  profile = "pneto-training"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
