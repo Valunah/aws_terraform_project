f9e9f75ef6580aabd33f432efcc0b242c2d1a477
Pedro Neto <pedroneto.cnm@gmail.com>
2024-11-07 17:17:37 +0000

    kill me

--- variables.tf
+++ variables.tf
@@ -1,12 +1,1 @@
-variable "aws_access_key" {
-  description = "AWS ACCESS KEY ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS_SECRET_ACCESS_KEY"
-  type        = string
-  sensitive   = true
-}
 
