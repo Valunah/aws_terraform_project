d61372ae960cf6836204a9e11bbac84be8df5094
Pedro Neto <pedroneto.cnm@gmail.com>
2024-11-07 17:01:34 +0000

    Lets use tfvars

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "AWS_ACCESS_KEY_ID" {
+variable "aws_access_key" {
   description = "AWS ACCESS KEY ID"
   type        = string
   sensitive   = true
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
+variable "aws_secret_key" {
   description = "AWS_SECRET_ACCESS_KEY"
   type        = string
   sensitive   = true
