69a95ec62d2be0899e0afabcacfff977be8c4334
Pedro Neto <pedroneto.cnm@gmail.com>
2024-11-07 16:34:18 +0000

    Set up access variables

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS ACCESS KEY ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS_SECRET_ACCESS_KEY"
+  type        = string
+  sensitive   = true
+}
+
